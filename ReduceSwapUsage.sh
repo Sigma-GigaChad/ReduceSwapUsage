afb0d0754efe2eca0c5cf17edd576c051ec0767c
delta-whiplash <inoushmolabaccus@gmail.com>
2022-12-04 13:09:57 +0100

    add some check

--- ReduceSwapUsage.sh
+++ ReduceSwapUsage.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-# ReduceSwapUsage.sh
+# check if you are root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# check if swap is enabled
+if [ "$(swapon -s | wc -l)" == "1" ]; then
+   echo "Swap is not enabled"
+   exit 0
+fi
+# check if swap is used
+if [ "$(free -m | grep Swap | awk '{print $3}')" == "0" ]; then
+   echo "Swap is not used"
+   exit 0
+fi
 echo "Your current swap is used when there is only `cat /proc/sys/vm/swappiness`% of ram left" 
 echo "Reducing swap usage..."
 sudo sysctl vm.swappiness=10
